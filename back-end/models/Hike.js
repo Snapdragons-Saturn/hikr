08da09fbc42c644ae646b494418bef1fb7d3497a
diakt <igs2107@columbia.edu>
2022-08-04 12:46:42 -0400

    HikeSchema defined

--- back-end/models/Hike.js
+++ back-end/models/Hike.js
@@ -2,7 +2,15 @@ const mongoose = require('../connection')
 
 
 const HikeSchema = new mongoose.Schema({
-        
+        hikeID: String,
+        stateAbb: String,
+        datetimeAdd: Number,
+        hikeName: String,
+        hikeDesc: String,
+        hikeDiff: String,
+        hikeLen: Number,
+        hkeTerrain: String,
+        userID: Number
 })
 
 const Hike = mongoose.model("Hike", HikesSchema)
